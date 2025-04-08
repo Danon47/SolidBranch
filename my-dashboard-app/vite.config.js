453b342ebf96645a151eb1af3fe6990535ac5826
Danon47 <danilshamray@gmail.com>
2025-04-08 22:33:23 +0300

    3

--- my-dashboard-app/vite.config.js
+++ my-dashboard-app/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons'],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
